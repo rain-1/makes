30ac41dee8bda661a44dad9cecd244d037918d43
rain1 <rain1@airmail.cc>
2018-04-10 23:13:32 +0100

    Update makes.c

--- makes.c
+++ makes.c
@@ -6,15 +6,27 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <limits.h>
+#include <libgen.h>
+#include <sys/inotify.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
 // makes output.o input-1.txt input-2.txt ...
 // return success (0) when
 //   output does not exist
 //   or any input is newer than the output
-// otherwise fails, signalling the need for a rebuild
+// otherwise fails with status (1), signalling that no build needs performed
+// if an error occurs status (2) will be produced
 
 #define NEXT do { argc--; argv++; } while(0)
 int main(int argc, char **argv) {
 	int verbose = 0;
+	int rebuild = 0;
 	struct stat s1, s2;
 	
 	NEXT;
@@ -22,20 +34,24 @@ int main(int argc, char **argv) {
 		verbose = 1;
 		NEXT;
 	}
+	if(getenv("MAKES_VERBOSE")) {
+		verbose = 1;
+	}
 	
 	if(stat(argv[0], &s1)) {
 		if(verbose) fprintf(stderr, "[makes] output file \"%s\" did not exist\n", argv[0]);
-		return 0;
+		rebuild = 1;
 	}
 	
 	for(int i = 1; i < argc; i++) {
 		if(!stat(argv[i], &s2)) {
 			if(s2.st_mtime > s1.st_mtime) {
 				if(verbose) fprintf(stderr, "[makes] input file \"%s\" was newer\n", argv[i]);
-				return 0;
+				rebuild = 1;
+
 			}
 		}
 	}
 	
-	return 1;
+	return !rebuild;
 }
